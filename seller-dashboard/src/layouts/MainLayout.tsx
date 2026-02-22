dec874071d41be18807a29d880f710a9be9b73a1
Sachin <sachinkumarsingh0029@gmail.com>
2026-02-22 23:59:09 +0530

    Fix async crash in MainLayout

--- seller-dashboard/src/layouts/MainLayout.tsx
+++ seller-dashboard/src/layouts/MainLayout.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import SideBar from "../component/sidebar/SideBar";
 import { Navigate, Outlet } from "react-router-dom";
-import Dashboard from "../pages/dashboard/Dashboard";
 import IpBanned from "../pages/error/IpBanned";
 import { logoutSuccess } from "../redux/user/userSlice";
 import { useDispatch, useSelector } from "react-redux";
@@ -15,48 +14,41 @@ const MainLayout = () => {
     (state: RootState) => state.user
   );
   const { seller } = useSelector((state: RootState) => state.seller);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
-    async function checkAuth() {
-      const response: any = await instance.get<any>("/auth/check");
-      return response;
-    }
+    const checkAuth = async () => {
+      try {
+        const response = await instance.get("/auth/check");
 
-    try {
-      const response: any = checkAuth();
-      if (response.data.status !== "success") {
+        if (response.data.status !== "success") {
+          dispatch(logoutSuccess());
+        }
+      } catch (err) {
         dispatch(logoutSuccess());
       }
-    } catch (err) {
-      // dispatch(logoutSuccess());
-    }
-  }, []);
+    };
+
+    checkAuth();
+  }, [dispatch]);
 
   if (ban?.status && ban?.banExpiresAt < Date.now()) {
     return <IpBanned />;
   }
 
-  if (!isAuthenticated && !user.verificationStatus && !seller) {
-    return <Navigate to="error" />;
-  } else if (isAuthenticated && !user.verificationStatus && seller) {
-    return <Navigate to="error" />;
+  if (!isAuthenticated) {
+    return <Navigate to="/error" replace />;
   }
 
   return (
-    <div>
-      <div className="flex h-screen">
-        <SideBar />
-        <span
-          className="overflow-scroll w-full"
-          // onClick={() => openSidebar === false && setOpenSidebar(true)}
-        >
-          <Outlet />
-        </span>
+    <div className="flex h-screen">
+      <SideBar />
+      <div className="overflow-auto w-full">
+        <Outlet />
       </div>
     </div>
   );
 };
 
 export default withAuth(MainLayout);
-// export default MainLayout;
