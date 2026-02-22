cd57aa03a536ab99f43ae18ad85a1fff5d5c2612
Sachin <sachinkumarsingh0029@gmail.com>
2026-02-23 03:16:44 +0530

    Fix MainLayout TypeScript build error

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,32 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/rootReducer";
-import instance from "../utils/Axios";
-import {
-  logoutSuccess,
-  banRemoved,
-  loginSuccess,
-} from "../redux/user/userSlice";
-import { useDispatch } from "react-redux";
 import IpBanned from "../pages/error/IpBanned";
 import HomeLayout from "./Homelayout";
 import FloatingButton from "../components/common/FloatingButton";
 import Chatbot from "../components/common/Chatbot";
 
-interface Props {
-  children: React.ReactNode;
-}
-
-interface AuthenticationResponse {
-  status: string;
-  message: string;
-}
-
-const MainLayout = ({ children }: Props) => {
-  const ban = useSelector((state: RootState) => state.user.ban);
-  const dispatch = useDispatch();
-  const { isAuthenticated, user } = useSelector(
+const MainLayout: React.FC = () => {
+  const { isAuthenticated, user, ban } = useSelector(
     (state: RootState) => state.user
   );
 
@@ -34,25 +16,19 @@ const MainLayout = ({ children }: Props) => {
     return <IpBanned />;
   }
 
-  // if (ban?.banExpiresAt < Date.now() && ban?.status) {
-  //   dispatch(banRemoved());
-  // }
-
   if (isAuthenticated && !user.verificationStatus) {
     return <Navigate to="/verification" />;
   }
 
   return (
-    <>
-      <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900">
-        <HomeLayout>
-          <Outlet />
-        </HomeLayout>
-        <FloatingButton />
-        <Chatbot />
-      </div>
-    </>
+    <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900">
+      <HomeLayout>
+        <Outlet />
+      </HomeLayout>
+      <FloatingButton />
+      <Chatbot />
+    </div>
   );
 };
 
-export default MainLayout;
+export default MainLayout;  
